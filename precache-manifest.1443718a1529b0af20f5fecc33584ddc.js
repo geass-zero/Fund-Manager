0d95c49877e719506d8d8fce9791c5c2badaeab4
geass-zero <nyanfinance@gmail.com>
2021-02-04 04:43:04 -0500

    Updates

---
+++ precache-manifest.1443718a1529b0af20f5fecc33584ddc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f272ab3d707f1d3aecc409c095d746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538afb5b638386a5c509",
+    "url": "/static/css/3.c6227911.chunk.css"
+  },
+  {
+    "revision": "805da1ad6d13160533d2",
+    "url": "/static/css/main.94bbb2f7.chunk.css"
+  },
+  {
+    "revision": "e4ce4617fe8788cb6b65",
+    "url": "/static/js/0.1e40ab43.chunk.js"
+  },
+  {
+    "revision": "4a419e52a5f1e6bd8b76f063e48c6e15",
+    "url": "/static/js/0.1e40ab43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538afb5b638386a5c509",
+    "url": "/static/js/3.96f09ebc.chunk.js"
+  },
+  {
+    "revision": "43c01f6048d54fb837561fe9ffb53bab",
+    "url": "/static/js/3.96f09ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2c049bd3e5da83667a",
+    "url": "/static/js/4.7d5fd94d.chunk.js"
+  },
+  {
+    "revision": "3861ef6037f2c6593a81",
+    "url": "/static/js/5.023b01fb.chunk.js"
+  },
+  {
+    "revision": "b7b85e4971d3a6086ff0",
+    "url": "/static/js/6.cfe003e4.chunk.js"
+  },
+  {
+    "revision": "5ad012a55cd22f602294",
+    "url": "/static/js/7.45bb0d29.chunk.js"
+  },
+  {
+    "revision": "805da1ad6d13160533d2",
+    "url": "/static/js/main.b09a8463.chunk.js"
+  },
+  {
+    "revision": "09e1e0f885e8aceccdcb",
+    "url": "/static/js/runtime-main.d5ef594c.js"
+  }
+]);
